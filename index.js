4c2ae9a722821a59551f7624d74266113484f762
Остапчук Алина <alinyonish@gmail.com>
2016-06-09 17:14:36 +0300

    tests & refactoring

--- index.js
+++ index.js
@@ -1,8 +1,105 @@
 'use strict';
 
-function resolve(config, caller, importing) {
-  return importing;
+function checkMap(config, caller, importing, root) {
+  let result = null;
+  if (config.map) {
+    Object.keys(config.map).forEach((key) => {
+      if (`${key}.js` === caller) {
+        Object.keys(config.map[key]).forEach((keyFile) => {
+          if (config.map[key][keyFile].indexOf(importing) !== -1) {
+            result = config.baseUrl
+              ? `${root}/${config.baseUrl}/${config.map[key][keyFile]}.js`
+              : `${root}/${config.map[key][keyFile]}.js`;
+          }
+        });
+      } else if ((`${key}.js` === '*') && !result) {
+        Object.keys(config.map[key]).forEach((keyFile) => {
+          if (config.map[key][keyFile].indexOf(importing) !== -1) {
+            result = config.baseUrl
+              ? `${root}/${config.baseUrl}/${config.map[key][keyFile]}.js`
+              : `${root}/${config.map[key][keyFile]}.js`;
+          }
+        });
+      }
+    });
+  }
+  return result;
+}
+
+function checkBundles(config, importing, prevResult, root) {
+  let result = null;
+  if (config.bundles && !prevResult) {
+    Object.keys(config.bundles).forEach((key) => {
+      if (config.bundles[key].indexOf(importing) !== -1) {
+        result = config.baseUrl ? `${root}/${config.baseUrl}/${key}.js` : `${root}/${key}.js`;
+      }
+    });
+  }
+  return result || prevResult;
+}
+
+function checkRootDirPath(importing, root, prevResult) {
+  let result = null;
+  if (importing.startsWith('/') && !prevResult) {
+    result = `${importing}`;
+  } else if (importing.endsWith('.js') && !prevResult) {
+    result = `${root}/${importing}`;
+  }
+  return result || prevResult;
+}
+
+function checkEmptyConfig(config, importing, root, prevResult) {
+  let result = null;
+  if (!config.baseUrl && !config.paths && !prevResult) {
+    result = `${root}/${importing}.js`;
+  }
+  return result || prevResult;
+}
+
+function checkRootPaths(config, importing, root, prevResult) {
+  let result = null;
+  if (config.paths && (config.paths[importing] === `../${importing}`) && !prevResult) { // todo: check
+    result = `${root}/${importing}.js`;
+  }
+  return result || prevResult;
+}
+
+function checkBaseUrlOnly(config, importing, root, prevResult) {
+  let result = null;
+  if (config.baseUrl && !config.paths && !prevResult) {
+    result = `${root}/${config.baseUrl}/${importing}.js`;
+  }
+  return result || prevResult;
+}
+
+function checkPathsOnly(config, importing, root, prevResult) {
+  let result = null;
+  if ((!config.baseUrl) && config.paths && config.paths[importing] && !prevResult) {
+    result = `${root}/${config.paths[importing]}.js`;
+  }
+  return result || prevResult;
+}
+
+function checkFullPath(config, importing, root, prevResult) {
+  let result = null;
+  if ((config.baseUrl) && config.paths && config.paths[importing] && !prevResult) {
+    result = `${root}/${config.baseUrl}/${config.paths[importing]}.js`;
+  }
+  return result || prevResult;
+}
+
+function resolveFile(config, caller, importing, root) {
+  let result;
+  result = checkMap(config, caller, importing, root);
+  result = checkBundles(config, importing, result, root);
+  result = checkRootDirPath(importing, root, result);
+  result = checkEmptyConfig(config, importing, root, result);
+  result = checkRootPaths(config, importing, root, result);
+  result = checkBaseUrlOnly(config, importing, root, result);
+  result = checkPathsOnly(config, importing, root, result);
+  result = checkFullPath(config, importing, root, result);
+  return result;
 }
 
 module.exports = {};
-module.exports.resolve = resolve;
+module.exports.resolve = resolveFile;
--- test/test.js
+++ test/test.js
@@ -1,24 +1,82 @@
 'use strict';
 const resolver = require('../index');
-
+const rootFolder = 'eslint-import-resolver-amd';
 describe('resolver', () => {
   it('resolver is not empty', () => {
     resolver.should.have.properties('resolve');
     resolver.resolve.should.be.type('function');
   });
-  /*  it('resolve with empty config', () =>
-   resolver.resolve({}, 'index.js', 'jquery-netu').should.be.equal('jquery-netu.js')
-   );
-   it('resolve with empty config', () =>
-   resolver.resolve({baseUrl: 'assets'}, 'index.js', 'jquery-netu').should.be.equal('assets/jquery-netu.js')
-   );
-   it('resolve with empty config', () =>
-   resolver.resolve({baseUrl: 'assets', paths: {app: '../app'}}, 'index.js', 'app').should.be.equal('app.js')
-   );
-   it('resolve with empty config', () =>
-   resolver.resolve({
-   baseUrl: 'assets',
-   paths  : {app: '../app'},
-   }, 'index.js', '/jquery-netu').should.be.equal('jquery-netu')
-   );*/
+  it('resolve with empty config', () =>
+      resolver.resolve({}, 'index.js', 'jquery-netu', rootFolder).should.be.equal(`${rootFolder}/jquery-netu.js`)
+  );
+  it('resolve with base url', () =>
+  resolver.resolve({
+    baseUrl: 'assets'},
+    'index.js', 'jquery-netu', rootFolder).should.be.equal(`${rootFolder}/assets/jquery-netu.js`)
+  );
+  it('resolve with root dir path', () =>
+  resolver.resolve({
+    baseUrl: 'assets',
+    paths: {app: '../app'}},
+    'index.js', 'app', rootFolder).should.be.equal(`${rootFolder}/app.js`)
+  );
+  it('resolve with special path and empty baseUrl', () =>
+  resolver.resolve({
+    paths: {foo: 'lib/foo2'},
+  }, 'index.js', 'foo', rootFolder).should.be.equal(`${rootFolder}/lib/foo2.js`)
+  );
+  it('resolve with special path', () =>
+  resolver.resolve({
+    baseUrl: 'assets',
+    paths: {foo: 'lib/foo2'},
+  }, 'index.js', 'foo', rootFolder).should.be.equal(`${rootFolder}/assets/lib/foo2.js`)
+  );
+  it('resolve with /**.js absolute path', () =>
+      resolver.resolve({
+        baseUrl: 'assets',
+        paths: {foo: 'lib/foo2'},
+      }, 'index.js', '/foo.js', rootFolder).should.be.equal('/foo.js')
+  );
+  it('resolve with / absolute path', () =>
+      resolver.resolve({
+        baseUrl: 'assets',
+        paths: {foo: 'lib/foo2'},
+      }, 'index.js', '/foo', rootFolder).should.be.equal('/foo')
+  );
+  it('resolve with .js path', () =>
+      resolver.resolve({
+        baseUrl: 'assets',
+        paths: {foo: 'lib/foo2'},
+      }, 'index.js', 'foo.js', rootFolder).should.be.equal(`${rootFolder}/foo.js`)
+  );
+  it('resolve bundles', () =>
+      resolver.resolve({
+        baseUrl: 'assets',
+        bundles: {
+          secondary: ['foo4', 'foo3', 'foo5'],
+          primary: ['foo1', 'foo2', 'foo'],
+        },
+      }, 'index.js', 'foo1', rootFolder).should.be.equal(`${rootFolder}/assets/primary.js`)
+  );
+  it('resolve bundles only', () =>
+      resolver.resolve({
+        bundles: {
+          secondary: ['foo4', 'foo3', 'foo5'],
+          primary: ['foo1', 'foo2', 'foo'],
+        },
+      }, 'index.js', 'foo1', rootFolder).should.be.equal(`${rootFolder}/primary.js`)
+  );
+  it('resolve map', () =>
+      resolver.resolve({
+        baseUrl: 'assets',
+        map: {
+          'some/oldmodule': {
+            foo: 'foo1.0',
+          },
+          index: {
+            foo: 'foo1.2',
+          },
+        },
+      }, 'index.js', 'foo', rootFolder).should.be.equal(`${rootFolder}/assets/foo1.2.js`)
+  );
 });
